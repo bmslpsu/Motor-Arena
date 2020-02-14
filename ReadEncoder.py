6cd4a9eefd70cf0db1bc55acc004482c02dd9aea
BC <boc5244@psu.edu>
2020-02-14 18:50:29 -0500

    PID

--- ReadEncoder.py
+++ ReadEncoder.py
@@ -1,45 +1,59 @@
 from Phidget22.Phidget import *
 from Phidget22.Devices.DCMotor import *
 from Phidget22.Devices.Encoder import *
+from simple_pid import PID
 import time
 
 class EncoderRead:
 	def __init__(self):
-		self.countprev = 2797
+		self.countprev = 700.8
 		self.totcount = 0
+		self.inivelocity = 0.5
+		self.pid = PID(0.001, 0, 0.00022, setpoint=700)
 
-	def printposition(self, positionChange, timeChange, indexTriggered,extra):
-		print("PositionChange: " + str(self.totcount))
-		print("TimeChange: " + str(timeChange))
-		print("IndexTriggered: " + str(indexTriggered))
-		print("----------")
-		self.totcount = self.encoder0.getPosition()
-		
-		
-	
-	def run(self):
+	def setup(self):
 		self.encoder0 = Encoder()
-		#self.dcMotor0 = DCMotor()
+		self.dcMotor0 = DCMotor()
+		
+
+		self.encoder0.openWaitForAttachment(5000)
+		self.dcMotor0.openWaitForAttachment(5000)
+		self.dcMotor0.setTargetVelocity(self.inivelocity)
 		
 		self.encoder0.setOnPositionChangeHandler(self.printposition)
 		
-		self.encoder0.openWaitForAttachment(5000)
-		#self.dcMotor0.openWaitForAttachment(5000)
-		
-		#self.dcMotor0.setTargetVelocity(0.1)
-		
-		
+
 		try:
 			input("Press Enter to Stop\n")
 		except (Exception, KeyboardInterrupt):
 			pass
 	
 		self.encoder0.close()
-		#self.dcMotor0.close()
+		self.dcMotor0.close()
+	
+	def printposition(self,positionChange, timeChange, indexTriggered, extra):
+		self.encoderread()
+		print("PositionChange: " + str(self.totcount))
+		print("TimeChange: " + str(self.control))
+		print("----------")
+		
+	def spiningmotor(self):
+		self.PIDposition(self.totcount)
+		self.dcMotor0.setTargetVelocity(self.control)
+	
+	def	encoderread(self):
+		self.totcount=self.encoder0.getPosition()
+		self.spiningmotor()
+		
+	def PIDposition(self,v):
+		self.control = self.pid(v)
+		
+	
+		
 		
 if __name__ == '__main__':
 	main = EncoderRead()
-	main.run()
+	main.setup()
 	
 	
 	
